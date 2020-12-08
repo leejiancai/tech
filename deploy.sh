0e22dc0d542973b1b18eb95d29379972054b3652
lijiancai <958000432@qq.com>
2020-12-08 21:35:37 +0800

    添加部署gitbook的脚本

---
+++ deploy.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+
+# 编译_book
+gitbook build
+
+# 切换到gh-pages分支
+git checkout gh-pages
+
+# 删除旧的book
+ls |grep -v "_book" |grep -v "node_modules" |xargs git rm -r
+
+# 添加新的book
+cp -r _book/* .
+git add *
+
+# commit 
+git commit -m "Publish gitbook in $(date)"
+
+# git push
+git push
+
+# 返回到master分支
+git checkout master
